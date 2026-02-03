27a026f81cc5b1b00085e8681572a3067d54a2c9
Ariana Nagumo <arinagumo@gmail.com>
2026-02-02 20:15:33 -0600

    feat(app): create project

---
+++ Spotijam/Spotijam/SpotijamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotijamApp.swift
+//  Spotijam
+//
+//  Created by Ariana Rodríguez on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotijamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spotijam/SpotijamUITests/SpotijamUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SpotijamUITestsLaunchTests.swift
+//  SpotijamUITests
+//
+//  Created by Ariana Rodríguez on 02/02/26.
+//
+
+import XCTest
+
+final class SpotijamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
